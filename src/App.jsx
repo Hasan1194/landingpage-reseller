72beda78d000bd70e5ac87ae56d54b4e40b67bd1
Hasan Abdurahman <hasanabdurahman336@gmail.com>
2025-12-28 23:37:17 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         <Route path="/signup" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        {/* Proteksi Role */}
         <Route
           path="/resellers"
           element={
